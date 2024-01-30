513b4f7f69ceed5dbcfb601798578cf08d245e3d
isamaulanatantra <isamaulanatantra@gmail.com>
2024-01-30 12:54:39 +0700

    kirim wa

--- app/Console/Commands/Perintah.php
+++ app/Console/Commands/Perintah.php
@@ -2,7 +2,9 @@
 
 namespace App\Console\Commands;
 
+use App\Models\Pegawai;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Http;
 
 class Perintah extends Command
 {
@@ -11,7 +13,7 @@ class Perintah extends Command
      *
      * @var string
      */
-    protected $signature = 'app:perintah';
+    protected $signature = 'perintah';
 
     /**
      * The console command description.
@@ -25,6 +27,28 @@ class Perintah extends Command
      */
     public function handle()
     {
-        //
+        $pegawai = Pegawai::with('relasinya')->get();
+
+        foreach ($pegawai as $value) {
+            $kirim = '';
+            $token = $value->token;
+
+            foreach ($value->relasinya as $value2) {
+                $response = Http::withHeaders([
+                    'Authorization' => 'Bearer ' . $token,
+                ])->get($value2->url);
+
+                $a = $response->object();
+                $kirim = $kirim . json_encode($a->result);
+            }
+
+            // kirim wa
+            Http::withHeaders([
+                'Authorization' => "y59Vw77031Tpb0Cv8dbO5QyJVKVKi6EXoWqHCVZaVDntDUIViSMQuWvz2jkNwePM",
+            ])->withoutVerifying()->post("https://pati.wablas.com/api/send-message", [
+                'phone' => $value->wa,
+                'message' => $kirim,
+            ]);
+        }
     }
 }
